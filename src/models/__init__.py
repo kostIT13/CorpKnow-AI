8e022a4ea9d81116bd9f1fac8d32e68e3b94aa21
Konstantin Balandin <kostabalandin169@gmail.com>
2026-03-25 22:13:36 +0700

    add module empty frontend

--- src/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.models.user import User
-from src.models.chat import Chat
-from src.models.message import Message
-from src.models.document import Document
-
-
-__all__ = ["User", "Chat", "Message", "Document"]
